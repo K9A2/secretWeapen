c2b986799947e7fbc1c83deedff9e47d573f07a1
喵 <376997313@qq.com>
2016-03-15 21:07:03 +0800

    UI设计完成
    主窗口的UI设计完成，等待填入各网站的业务逻辑代码

---
+++ secretWeapen/secretWeapen/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace secretWeapen
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.cbo_site = new System.Windows.Forms.ComboBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_input
+            // 
+            this.txt_input.Location = new System.Drawing.Point(10, 10);
+            this.txt_input.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(300, 25);
+            this.txt_input.TabIndex = 0;
+            // 
+            // cbo_site
+            // 
+            this.cbo_site.FormattingEnabled = true;
+            this.cbo_site.Items.AddRange(new object[] {
+            "Btspread",
+            "TorrentKitty",
+            "BtDigg"});
+            this.cbo_site.Location = new System.Drawing.Point(320, 10);
+            this.cbo_site.Margin = new System.Windows.Forms.Padding(5);
+            this.cbo_site.Name = "cbo_site";
+            this.cbo_site.Size = new System.Drawing.Size(120, 23);
+            this.cbo_site.TabIndex = 1;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(450, 9);
+            this.btn_search.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(120, 23);
+            this.btn_search.TabIndex = 2;
+            this.btn_search.Text = "搜索";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(560, 300);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 355);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.cbo_site);
+            this.Controls.Add(this.txt_input);
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Main";
+            this.Text = "搜片神器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox textBox1;
+        protected internal System.Windows.Forms.ComboBox cbo_site;
+    }
+}
+
