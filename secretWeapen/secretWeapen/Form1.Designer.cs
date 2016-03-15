617dc4acb39d57f57c1572a0d36bb75d98ebd216
喵 <376997313@qq.com>
2016-03-15 22:57:08 +0800

    成功获取源代码
    getHtmlCode编写成功

--- secretWeapen/secretWeapen/Form1.Designer.cs
+++ secretWeapen/secretWeapen/Form1.Designer.cs
@@ -29,9 +29,11 @@
         private void InitializeComponent()
         {
             this.txt_input = new System.Windows.Forms.TextBox();
-            this.cbo_site = new System.Windows.Forms.ComboBox();
             this.btn_search = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.rdo_bts = new System.Windows.Forms.RadioButton();
+            this.rdo_tor = new System.Windows.Forms.RadioButton();
+            this.rdo_btd = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // txt_input
@@ -39,54 +41,82 @@
             this.txt_input.Location = new System.Drawing.Point(10, 10);
             this.txt_input.Margin = new System.Windows.Forms.Padding(5);
             this.txt_input.Name = "txt_input";
-            this.txt_input.Size = new System.Drawing.Size(300, 25);
+            this.txt_input.Size = new System.Drawing.Size(460, 25);
             this.txt_input.TabIndex = 0;
-            // 
-            // cbo_site
-            // 
-            this.cbo_site.FormattingEnabled = true;
-            this.cbo_site.Items.AddRange(new object[] {
-            "Btspread",
-            "TorrentKitty",
-            "BtDigg"});
-            this.cbo_site.Location = new System.Drawing.Point(320, 10);
-            this.cbo_site.Margin = new System.Windows.Forms.Padding(5);
-            this.cbo_site.Name = "cbo_site";
-            this.cbo_site.Size = new System.Drawing.Size(120, 23);
-            this.cbo_site.TabIndex = 1;
             // 
             // btn_search
             // 
-            this.btn_search.Location = new System.Drawing.Point(450, 9);
+            this.btn_search.Location = new System.Drawing.Point(480, 9);
             this.btn_search.Margin = new System.Windows.Forms.Padding(5);
             this.btn_search.Name = "btn_search";
-            this.btn_search.Size = new System.Drawing.Size(120, 23);
+            this.btn_search.Size = new System.Drawing.Size(90, 23);
             this.btn_search.TabIndex = 2;
             this.btn_search.Text = "搜索";
             this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
             // 
-            // textBox1
+            // txt_result
             // 
-            this.textBox1.Location = new System.Drawing.Point(10, 45);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(560, 300);
-            this.textBox1.TabIndex = 3;
+            this.txt_result.Location = new System.Drawing.Point(10, 72);
+            this.txt_result.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_result.Multiline = true;
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(560, 273);
+            this.txt_result.TabIndex = 3;
+            // 
+            // rdo_bts
+            // 
+            this.rdo_bts.AutoSize = true;
+            this.rdo_bts.Location = new System.Drawing.Point(12, 43);
+            this.rdo_bts.Margin = new System.Windows.Forms.Padding(5);
+            this.rdo_bts.Name = "rdo_bts";
+            this.rdo_bts.Size = new System.Drawing.Size(92, 19);
+            this.rdo_bts.TabIndex = 4;
+            this.rdo_bts.TabStop = true;
+            this.rdo_bts.Text = "Btspread";
+            this.rdo_bts.UseVisualStyleBackColor = true;
+            this.rdo_bts.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdo_tor
+            // 
+            this.rdo_tor.AutoSize = true;
+            this.rdo_tor.Location = new System.Drawing.Point(110, 43);
+            this.rdo_tor.Margin = new System.Windows.Forms.Padding(5);
+            this.rdo_tor.Name = "rdo_tor";
+            this.rdo_tor.Size = new System.Drawing.Size(124, 19);
+            this.rdo_tor.TabIndex = 5;
+            this.rdo_tor.TabStop = true;
+            this.rdo_tor.Text = "TorrentKitty";
+            this.rdo_tor.UseVisualStyleBackColor = true;
+            // 
+            // rdo_btd
+            // 
+            this.rdo_btd.AutoSize = true;
+            this.rdo_btd.Location = new System.Drawing.Point(244, 43);
+            this.rdo_btd.Margin = new System.Windows.Forms.Padding(5);
+            this.rdo_btd.Name = "rdo_btd";
+            this.rdo_btd.Size = new System.Drawing.Size(76, 19);
+            this.rdo_btd.TabIndex = 6;
+            this.rdo_btd.TabStop = true;
+            this.rdo_btd.Text = "BtDigg";
+            this.rdo_btd.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 355);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rdo_btd);
+            this.Controls.Add(this.rdo_tor);
+            this.Controls.Add(this.rdo_bts);
+            this.Controls.Add(this.txt_result);
             this.Controls.Add(this.btn_search);
-            this.Controls.Add(this.cbo_site);
             this.Controls.Add(this.txt_input);
             this.MaximumSize = new System.Drawing.Size(600, 400);
             this.MinimumSize = new System.Drawing.Size(600, 400);
             this.Name = "Main";
             this.Text = "搜片神器";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,8 +126,10 @@
 
         private System.Windows.Forms.TextBox txt_input;
         private System.Windows.Forms.Button btn_search;
-        private System.Windows.Forms.TextBox textBox1;
-        protected internal System.Windows.Forms.ComboBox cbo_site;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.RadioButton rdo_bts;
+        private System.Windows.Forms.RadioButton rdo_tor;
+        private System.Windows.Forms.RadioButton rdo_btd;
     }
 }
 
--- secretWeapen/secretWeapen/Form1.cs
+++ secretWeapen/secretWeapen/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
 
 namespace secretWeapen
@@ -15,6 +10,62 @@ namespace secretWeapen
         public Main()
         {
             InitializeComponent();
+        }
+
+        private const string BTS_MAIN = "http://www.btaia.com/search/";
+
+        private const string TOR_MAIN = "";
+
+        private const string BTD_MAIN = "";
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            if (txt_input.Text == "")
+            {
+                MessageBox.Show("请输入搜索关键字");
+            }
+            else if (rdo_bts.Checked == false && rdo_btd.Checked == false && rdo_tor.Checked == false)
+            {
+                MessageBox.Show("请选择搜索网站");
+            }
+            else
+            {
+                txt_result.Clear();
+                string keyword = txt_input.Text.ToString();
+                
+            }
+        }
+
+        private string getHtmlCode(string url)
+        {
+            Uri uri = new Uri(url);
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(uri);
+            myReq.UserAgent = "Mozilla / 5.0(Windows NT 6.1; WOW64; rv: 45.0) Gecko / 20100101 Firefox / 45.0";
+            myReq.Accept = "*/*";
+            myReq.KeepAlive = true;
+            myReq.Headers.Add("Accept-Language", "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3");
+            HttpWebResponse result = (HttpWebResponse)myReq.GetResponse();
+            Stream receviceStream = result.GetResponseStream();
+            StreamReader readerOfStream = new StreamReader(receviceStream, System.Text.Encoding.GetEncoding("utf-8"));
+            string strHTML = readerOfStream.ReadToEnd();
+            readerOfStream.Close();
+            receviceStream.Close();
+            result.Close();
+            return strHTML;
+        }
+
+
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
